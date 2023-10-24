5dca3c9870d8ffacc781d08b9a5ce8b749cd5d3c
Andrii Batutin <andrii.batutin@shelf.io>
2023-10-24 14:28:36 +0200

    addes breadth and depth first searches

--- src/task/max_depth_104/bin_tree_max_depth.py
+++ src/task/max_depth_104/bin_tree_max_depth.py
@@ -17,6 +17,47 @@ class Solution:
 
     queue_n = []
 
+    def __init__(self) -> None:
+        self.res = []
+
+    def has_path(self, src: Optional[TreeNode], dst: Optional[TreeNode]):
+        pass
+
+    def breadth_first(self, root: Optional[TreeNode]):
+        q = [root]
+
+        while q:
+            current = q.pop(0)
+            self.res.append(current.val)
+            if current.left:
+                q.append(current.left)
+            if current.right:
+                q.append(current.right)
+    
+
+    def depth_first(self, root: Optional[TreeNode]):
+        s_stack = [root]
+
+        while s_stack:
+            current = s_stack.pop() #  this is the only difference between breadth and depth first, pop vs pop(0)
+            print(current.val)
+            self.res.append(current.val)
+            if current.right:
+                s_stack.append(current.right)
+            if current.left:
+                s_stack.append(current.left)
+
+    
+    def depth_first_recursive(self, root: Optional[TreeNode]):
+        
+        if root is None:
+            return
+        print(root.val)
+        self.res.append(root.val)
+        self.depth_first_recursive(root.left)
+        self.depth_first_recursive(root.right)
+
+
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0        
--- tests/unit/test_solutions.py
+++ tests/unit/test_solutions.py
@@ -5,7 +5,7 @@ from src.task.max_depth_104.bin_tree_max_depth import Solution, TreeNode
 
 @pytest.fixture
 def mock_adj_list():
-    return {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": [], "e": ["b"], "f": ["d"]}
+    return {"a": ["b", "c"], "b": ["d"], "c": ["e"], "d": ["f"], "e": [], "f": []}
 
 
 @pytest.fixture
@@ -29,3 +29,22 @@ def convert_to_tree(adj_list, root):
 def test_104(mock_tree):
     s = Solution()
     assert s.maxDepth(mock_tree) == 5
+
+def test_depth_first(mock_tree):
+    s = Solution()
+    s.depth_first(mock_tree)
+    assert s.res == ["a", "b", "d", "f", "c", "e"]
+    s = Solution()
+    s.depth_first_recursive(mock_tree) 
+    assert s.res == ["a", "b", "d", "f", "c", "e"]
+    
+def test_depth_first_recursive(mock_tree):
+    s = Solution()
+    s.depth_first_recursive(mock_tree) 
+    assert s.res == ["a", "b", "d", "f", "c", "e"]
+
+def test_breadth_first(mock_tree):
+    s = Solution()
+    s.breadth_first(mock_tree) 
+    assert s.res == ["a", "b", "c", "d", "e", "f"]
+
